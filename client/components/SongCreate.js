664c539fb01caae5e131f0868c3abc839688e65b
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-02-17 21:23:51 +0900

    Lecture 46, Defining Query Variables in React

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -7,11 +9,20 @@ class SongCreate extends Component {
     this.state = { title: '' };
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.mutate({
+      variables: {
+        title: this.state.title,
+      }
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>Create a New Song</h3>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
           <input
             type="text"
@@ -24,4 +35,13 @@ class SongCreate extends Component {
   }
 }
 
-export default SongCreate;
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);

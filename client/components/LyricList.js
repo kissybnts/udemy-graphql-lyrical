05f1937349d49bcbc0ed838df0727055d22dd248
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-02-18 13:06:43 +0900

    Lecture 63, Showing a List of Lyrics

---
+++ client/components/LyricList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class LyricList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Lyric</li>
+      </ul>
+    );
+  }
+}
+
+export default LyricList;
